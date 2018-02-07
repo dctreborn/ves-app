a521c348d77589cd5ceb110a27b12778cfd9b1a1
rukaroa <rukaroa@gmail.com>
2018-02-07 10:03:36 -0600

    post request added

--- server/src/app.js
+++ server/src/app.js
@@ -16,4 +16,10 @@ app.get('/status', (req, res) => {
     })
 })
 
+app.post('/register', (req, res) => {
+    res.send({
+        message: `User registered! ${req.body}`
+    })
+})
+
 app.listen(process.env.PORT || 8081)
