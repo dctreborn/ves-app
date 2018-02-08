ce1ea8049d0859913ec965383360bc5edb4ac89d
rukaroa <rukaroa@gmail.com>
2018-02-08 08:25:25 -0600

    added sequelize and sync

--- server/src/app.js
+++ server/src/app.js
@@ -1,25 +1,19 @@
-console.log('server start')
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const {sequelize} = require('./models')
+
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
-    res.send({
-        message: 'hello world!'
-    })
-})
+require('./routes')(app)
 
-app.post('/register', (req, res) => {
-    res.send({
-        message: `User registered! ${req.body}`
-    })
+sequelize.sync().then(()=>{
+    app.listen(config.port)
+    console.log(`Server started on port ${config.port}`)
 })
-
-app.listen(process.env.PORT || 8081)
