3789d737b5496e5dbcb74647c7ce7178c2ddf50f
rukaroa <rukaroa@gmail.com>
2018-02-09 12:34:35 -0600

    header component created with basic layout

---
+++ client/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-toolbar dark fixed class="cyan">
+        <v-toolbar-title class="mr-4">
+            Tab Tracker
+        </v-toolbar-title>
+
+        <v-toolbar-items>
+            <v-btn flat dark>
+                Browser
+            </v-btn>
+        </v-toolbar-items>
+
+        <v-spacer/>
+
+        <v-toolbar-items>
+            <v-btn flat dark>
+                Sign Up
+            </v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
