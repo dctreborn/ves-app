b401d1504ed6579bedc7f3980e021296e1939f97
rukaroa <rukaroa@gmail.com>
2018-02-09 11:50:33 -0600

    display error code to screen

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -6,6 +6,7 @@
       <br>
       <input type="password" name="password" v-model="password" placeholder="password"/>
       <br>
+      <div class="error" v-html="error"/>
       <button @click="register">Register</button>
   </div>
 </template>
@@ -17,21 +18,27 @@ export default {
   data() {
       return {
           email: '',
-          password: ''
+          password: '',
+          error: null
       }
   },
   methods: {
     async register(){
-      const response = await authenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await authenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+  .error {
+    color: red;
+  }
 </style>
