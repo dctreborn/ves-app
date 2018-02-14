69da025cb3d0864d1b44bc28dd5ce835840a7014
rukaroa <rukaroa@gmail.com>
2018-02-14 07:45:31 -0600

    login component and route added

---
+++ client/src/components/LogIn.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+
+        <div class="pl-4 pr-4 pt-2 pb-2" >
+          <v-text-field
+              name="email"
+              box
+              label="Email"
+              placeholder="email"
+              v-model="email"
+            ></v-text-field>
+          <br>
+          <v-text-field
+              name="password"
+              label="Password"
+              box
+              placeholder="password"
+              v-model="password"
+            ></v-text-field>
+          <br>
+          <div class="error" v-html="error"/>
+          <v-btn class="cyan" @click="register">Register</v-btn>
+        </div>        
+
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import authenticationService from '@/services/authenticationService'
+
+export default {
+  data() {
+      return {
+          email: '',
+          password: '',
+          error: null
+      }
+  },
+  methods: {
+    async register(){
+      try {
+        await authenticationService.login({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .error {
+    color: red;
+  }
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Register from '@/components/Register'
+import LogIn from '@/components/LogIn'
 
 Vue.use(Router)
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/register',
       name: 'register',
       component: Register
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LogIn
     }
   ]
 })
