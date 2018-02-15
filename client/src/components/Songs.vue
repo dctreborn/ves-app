dcd6d54100988ab6f81d7ea97692c74a1c4623a2
rukaroa <rukaroa@gmail.com>
2018-02-15 09:34:29 -0600

    song listing started

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -1,7 +1,13 @@
 <template>
   <v-layout column>
     <v-flex xs6 offset-xs3>
-      <panel title="Songs"/>
+      <panel title="Songs">
+          <div v-for="song in songs" :key="song.title">
+              {{song.title}} - 
+              {{song.artist}} - 
+              {{song.album}}
+          </div>
+      </panel>
     </v-flex>
   </v-layout>
 </template>
@@ -11,6 +17,14 @@ import Panel from '@/components/Panel'
 export default {
   components: {
       Panel
+  },
+  data (){
+      return {
+          songs: []
+      }
+  },
+  mounted(){
+    //   request backend data
   }
 }
 </script>
