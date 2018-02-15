1b44aeb2d8398b35c6245014549ced37b8e33330
rukaroa <rukaroa@gmail.com>
2018-02-15 11:53:06 -0600

    panel modified

--- client/src/components/Panel.vue
+++ client/src/components/Panel.vue
@@ -1,20 +1,16 @@
 <template>
-  <v-layout column>
-    <v-flex xs6 offset-xs3>
-      <div class="white elevation-2">
-        <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>{{title}}</v-toolbar-title>
-        </v-toolbar>
+  <div class="white elevation-2">
+    <v-toolbar flat dense class="cyan" dark>
+      <v-toolbar-title>{{title}}</v-toolbar-title>
+    </v-toolbar>
 
-        <div class="pl-4 pr-4 pt-2 pb-2" >
-          <slot>
-              No slot content yet
-          </slot>
-        </div>        
+    <div class="pl-4 pr-4 pt-2 pb-2" >
+      <slot>
+          No slot content yet
+      </slot>
+    </div>        
 
-      </div>
-    </v-flex>
-  </v-layout>
+  </div>
 </template>
 
 <script>
