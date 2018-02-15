25fee54978563edc09385722f3a4dfb7cca40eed
rukaroa <rukaroa@gmail.com>
2018-02-15 08:02:37 -0600

    added const response

--- client/src/components/LogIn.vue
+++ client/src/components/LogIn.vue
@@ -47,10 +47,12 @@ export default {
   methods: {
     async register(){
       try {
-        await authenticationService.login({
+        const response = await authenticationService.login({
           email: this.email,
           password: this.password
         })
+        this.$store.dispatch('setToken', response.data.token)
+        this.$store.dispatch('setUser', response.data.user)
       } catch (error) {
         this.error = error.response.data.error
       }
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -50,10 +50,12 @@ export default {
   methods: {
     async register(){
       try {
-        await authenticationService.register({
+        const response = await authenticationService.register({
           email: this.email,
           password: this.password
         })
+        this.$store.dispatch('setToken', response.data.token)
+        this.$store.dispatch('setUser', response.data.user)
       } catch (error) {
         this.error = error.response.data.error
       }
