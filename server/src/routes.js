2de66e60dbc184e06f2ce02ca79a9c3201185b20
rukaroa <rukaroa@gmail.com>
2018-02-15 09:46:59 -0600

    song post route added

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,12 +1,16 @@
 const AuthenticationController = require('./controllers/AuthenticationController')
-
 const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+const SongsController = require('./policies/SongsController')
 
 module.exports  = (app) => {
     app.post('/register',        
         // policy must be first for validation
         AuthenticationControllerPolicy.register,
-        AuthenticationController.register),
+        AuthenticationController.register)
+
     app.post('/login',
         AuthenticationController.login)
+
+    app.post('/songs',
+        SongsController.index)
 }
