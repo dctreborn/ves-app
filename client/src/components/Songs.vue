e27b4bc34535f79a5769fc81729090f0da7b9738
rukaroa <rukaroa@gmail.com>
2018-02-16 09:13:56 -0600

    refactored song list for better viewing

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -13,10 +13,33 @@
                 fab>
                 <v-icon>+</v-icon>
             </v-btn>
-          <div v-for="song in songs" :key="song.id">
-              {{song.title}} - 
-              {{song.artist}} - 
-              {{song.album}}
+          <div v-for="song in songs" class="song" :key="song.id">
+
+              <v-layout>
+                  <v-flex xs6>
+                    <div class="song-title">
+                        {{song.title}}
+                    </div>
+                    <div class="song-artist">
+                        {{song.artist}}
+                    </div>
+                    <div class="song-genre">
+                        {{song.genre}}
+                    </div>
+
+                    <v-btn
+                        dark
+                        class="cyan"
+                        @click="navigateTo({name: 'song', params: {songId: song.id}})"
+                    >
+                        View
+                    </v-btn>
+
+                  </v-flex>
+                  <v-flex xs6>
+                      <img :src="song.albumImageUrl" class="album-image">
+                  </v-flex>
+              </v-layout>
           </div>
       </panel>
     </v-flex>
@@ -48,5 +71,25 @@ export default {
 </script>
 
 <style scoped>
+.song {
+    padding: 20px;
+    height: 330px;
+    overflow: hidden;
+}
 
+.song-title {
+    font-size: 30px;
+}
+
+.song-artist {
+    font-size: 24;
+}
+
+.song-tigenretle {
+    font-size: 18;
+}
+
+.album-image {
+    width: 100%;
+}
 </style>
