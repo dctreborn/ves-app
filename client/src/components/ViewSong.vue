503c6431ff80070ba20ccec2f24dcf62569967e9
rukaroa <rukaroa@gmail.com>
2018-02-16 09:23:07 -0600

    variable fix

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -7,7 +7,7 @@ import songsService from '@/services/songsService'
 export default {    
     async mounted(){
         const songId = this.$store.state.route.params.songId
-        const song = await songsService.show(showId)
+        const song = await songsService.show(songId)
     }
 }
 </script>
