589a6472225ee0ce08f8975d3dd89dbd0032abb9
rukaroa <rukaroa@gmail.com>
2018-02-16 12:55:25 -0600

    edit song component created

---
+++ client/src/components/EditSong.vue
@@ -0,0 +1,140 @@
+<template>
+<v-layout>
+    <v-flex xs4>
+        <panel title="Song Metadata">                
+            <v-text-field
+                label="Title"
+                required
+                :rules="[required]"
+                v-model="song.title"
+            ></v-text-field>
+
+            <v-text-field
+                label="Artist"
+                required
+                :rules="[required]"
+                v-model="song.artist"
+            ></v-text-field>
+
+            <v-text-field
+                label="Genre"
+                required
+                :rules="[required]"
+                v-model="song.genre"
+            ></v-text-field>
+
+            <v-text-field
+                label="Album"
+                required
+                :rules="[required]"
+                v-model="song.album"
+            ></v-text-field>
+
+            <v-text-field
+                label="Image URL"
+                required
+                :rules="[required]"
+                v-model="song.albumImageUrl"
+            ></v-text-field>
+
+            <v-text-field
+                label="Youtube ID"
+                required
+                :rules="[required]"
+                v-model="song.youtubeId"
+            ></v-text-field>
+                 
+        </panel>
+        </v-flex>
+        
+        <v-flex xs8>
+            <panel title="Song Structure" class="ml-2">            
+                <v-text-field
+                    label="Lyrics"
+                    multi-line
+                    required
+                    :rules="[required]"
+                    v-model="song.lyrics"
+                ></v-text-field>
+
+                <v-text-field
+                    label="Tab"
+                    multi-line
+                    required
+                    :rules="[required]"
+                    v-model="song.tab"
+                ></v-text-field>  
+            </panel>
+            
+            <div class="danger-alert" v-if="error">
+                {{error}}
+            </div>
+            <v-btn class="cyan" @click="save">Update Song</v-btn>
+        </v-flex>    
+  </v-layout>
+</template>
+
+<script>
+import songsService from '@/services/songsService'
+import Panel from '@/components/Panel'
+export default {
+  data(){
+      return {        
+        song: {
+            title: null,
+            artist: null,
+            genre: null,
+            album: null,
+            albumImageUrl: null,
+            youtubeId: null,
+            lyrics: null,
+            tab: null
+        },
+        required: (value) => !!value || 'Required',
+        error: null
+      }
+  },
+  methods: {
+      async save(){
+        this.error = null
+        const allFieldsFilled = Object
+            .keys(this.song)
+            .every(key => !!this.song[key])
+        if (!allFieldsFilled) {
+            this.error = 'Please fill in all the required fields'
+            return
+        }
+
+        try {
+            const songId = this.$store.state.route.params.songId
+            await songsService.put(this.song) 
+            this.$router.push({
+                name: 'song',
+                params: {
+                    songId: songId
+                }
+            })
+        } catch (err) {
+            console.log(err)
+        }
+        
+      }
+  },
+  async mounted (){
+      try {
+        const songId = this.$store.state.route.params.songId
+        this.song = (await songsService.show(songId)).data
+      } catch (err) {
+          console.log(err)
+      }
+      
+  },
+  components: {
+      Panel
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
