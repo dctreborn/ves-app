1a0117a1387b3e4a8f5ad3618b6ffb2affa75542
rukaroa <rukaroa@gmail.com>
2018-02-16 11:00:08 -0600

    view song created

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -1,17 +1,69 @@
 <template>
-  
+    <v-layout>
+        <v-flex xs6>
+            <panel title="Song Data">
+                <v-layout>
+                    <v-flex xs6>
+                        <div class="song-title">
+                            {{song.title}}
+                        </div>
+                        <div class="song-artist">
+                            {{song.artist}}
+                        </div>
+                        <div class="song-genre">
+                            {{song.genre}}
+                        </div>
+                    </v-flex>
+                    
+                    <v-flex xs6>
+                        <img :src="song.albumImageUrl" class="album-image">
+                        {{song.album}}
+                    </v-flex>
+                </v-layout>
+            </panel>
+        </v-flex>
+
+        <v-flex xs6 class="ml-2">
+            <panel title="Tabs">
+                {{song.tab}}
+            </panel>
+        </v-flex>
+    </v-layout>    
 </template>
 
 <script>
 import songsService from '@/services/songsService'
-export default {    
+import Panel from '@/components/Panel'
+export default {
+    data(){
+        return {
+            song: {}
+        }
+    },    
     async mounted(){
         const songId = this.$store.state.route.params.songId
-        const song = await songsService.show(songId)
+        this.song = (await songsService.show(songId)).data
+    },
+    components: {
+        Panel
     }
 }
 </script>
 
 <style scoped>
+.song-title {
+    font-size: 30px;
+}
 
+.song-artist {
+    font-size: 24;
+}
+
+.song-genre {
+    font-size: 18;
+}
+
+.album-image {
+    width: 100%;
+}
 </style>
