f843e03209ba50ee1526d970dab79cc3ad238a22
rukaroa <rukaroa@gmail.com>
2018-02-19 12:41:18 -0600

    query watch and removed mount

--- client/src/components/Songs/SongsPanel.vue
+++ client/src/components/Songs/SongsPanel.vue
@@ -55,9 +55,13 @@ export default {
           this.$router.push(route)
       }
   },
-  async mounted(){
-    //   request backend data
-    this.songs = (await SongsService.index()).data
+  watch: {
+      '$route.query.search': {
+            immediate: true,
+            async handler(value){
+                this.songs = (await SongsService.index(value)).data
+            }
+        }
   }
 }
 </script>
