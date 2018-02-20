b6a1f50f3a9c7fdd480df9166647bf1159df1ce4
rukaroa <rukaroa@gmail.com>
2018-02-20 12:38:10 -0600

    create bookmarks from seed

--- server/seed/index.js
+++ server/seed/index.js
@@ -1,8 +1,9 @@
-const { sequelize, Song, User } = require('../src/models')
+const { sequelize, Song, User, Bookmark } = require('../src/models')
 
 const Promise = require('bluebird')
 const songs = require('./songs.json')
 const users = require('./users.json')
+const bookmarks = require('./bookmarks.json')
 
 sequelize.sync({ force: true}).then(async function() {
     await Promise.all(
@@ -14,6 +15,12 @@ sequelize.sync({ force: true}).then(async function() {
     await Promise.all(
         songs.map(song => {
             Song.create(song)
+        })
+    )
+
+    await Promise.all(
+        bookmarks.map(bookmark => {
+            Bookmark.create(bookmark)
         })
     )
 })
