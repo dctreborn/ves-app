7030c00a0969f157615d442bfeda97ea0af04caf
rukaroa <rukaroa@gmail.com>
2018-02-22 11:26:36 -0600

    history seed

--- server/seed/index.js
+++ server/seed/index.js
@@ -1,9 +1,16 @@
-const { sequelize, Song, User, Bookmark } = require('../src/models')
+const { 
+    sequelize,
+    Song,
+    User,
+    Bookmark,
+    History
+} = require('../src/models')
 
 const Promise = require('bluebird')
 const songs = require('./songs.json')
 const users = require('./users.json')
 const bookmarks = require('./bookmarks.json')
+const histories = require('./history.json')
 
 sequelize.sync({ force: true}).then(async function() {
     await Promise.all(
@@ -21,6 +28,12 @@ sequelize.sync({ force: true}).then(async function() {
     await Promise.all(
         bookmarks.map(bookmark => {
             Bookmark.create(bookmark)
+        })
+    )
+
+    await Promise.all(
+        histories.map(history => {
+            History.create(history)
         })
     )
 })
