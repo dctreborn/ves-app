6291d70aad7f4762df2eb69acef837ff9cc2ff14
rukaroa <rukaroa@gmail.com>
2018-02-23 09:31:14 -0600

    added authentication check using tokens

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,6 +3,7 @@ const AuthenticationControllerPolicy = require('./policies/AuthenticationControl
 const SongsController = require('./controllers/SongsController')
 const BookmarksController = require('./controllers/BookmarksController')
 const HistoriesController = require('./controllers/HistoriesController')
+const isAuthenticated = require('./policies/isAuthenticated')
 
 module.exports  = (app) => {
     app.post('/register',        
@@ -23,6 +24,7 @@ module.exports  = (app) => {
         SongsController.put)
 
     app.get('/bookmarks',
+        isAuthenticated,
         BookmarksController.index)
     app.post('/bookmarks',
         BookmarksController.post)
